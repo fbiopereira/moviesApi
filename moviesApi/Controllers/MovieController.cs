b7355d597b264596e69fbe04f66da7b1efc77af3
Fábio Pereira <fabio.pereira@outlook.com>
2023-06-09 16:05:31 -0300

    Class 3 - retrieve, pagination and httpstatus code

--- moviesApi/Controllers/MovieController.cs
+++ moviesApi/Controllers/MovieController.cs
@@ -1,18 +1,44 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Mvc;
 using moviesApi.Models;
 
 namespace moviesApi.Controllers;
 
 [ApiController]
-[Route("[controller")]
-public class MovieController
+[Route("[controller]")]
+public class MovieController: ControllerBase
 {
-
-    public void AddMovie(Movie movie)
+    private static List<Movie> movies = new List<Movie>();
+    private static int id = 0;
+    
+    [HttpPost]
+    public IActionResult AddNewMovie([FromBody] Movie movie)
     {
-          // add movie to database
+        movie.Id = ++id; 
+        movies.Add(movie);
+        return CreatedAtAction(nameof(GetMovieById), 
+            new {id = movie.Id}, 
+            movie);
+
     }
 
+    [HttpGet]
+    public IEnumerable<Movie> GetMovies([FromQuery] int page=0, [FromQuery] int pageSize = 10)
+    {
+        return movies.Skip(pageSize * page).Take(pageSize);
+            
+    }
 
+    [HttpGet("{id}")]
+    public IActionResult GetMovieById(int id)
+    {
+        var movie = movies.FirstOrDefault(movie => movie.Id == id);
+        if (movie == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(movie);
+
+    }
 
 }
--- moviesApi/Models/Movie.cs
+++ moviesApi/Models/Movie.cs
@@ -1,5 +1,24 @@
-﻿namespace moviesApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace moviesApi.Models;
 
 public class Movie
 {
+
+    public int Id { get; set; }
+
+    [Required(ErrorMessage ="Movie Title is required")]
+    [MaxLength(50, ErrorMessage ="Movie Title must not exceed 50 characters")]
+    [Display(Name ="Title")]
+    public string Title { get; set; }
+    
+    [Required(ErrorMessage ="Movie Genre is required")]
+    [MaxLength(30, ErrorMessage ="Movie Genre must not exceed 30 characters")]
+    [Display(Name ="Genre")]        
+    public string Genre { get; set; }
+    
+    [Required(ErrorMessage ="Movie duration is required")]
+    [Range(1,300, ErrorMessage ="Movie Duration must be between 1 and 300 minutes")]
+    [Display(Name ="Duration (minutes)")]
+    public int Duration { get; set; }
 }
