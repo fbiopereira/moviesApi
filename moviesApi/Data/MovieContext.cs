8bcd69700b3bbf1e0fd78e855110a25475c6ad94
Fábio Pereira <fabio.pereira@outlook.com>
2023-06-15 15:03:58 -0300

    Class 4: Adding database do POST method

---
+++ moviesApi/Data/MovieContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+using moviesApi.Models;
+
+namespace moviesApi.Data;
+
+public class MovieContext : DbContext
+{
+    public MovieContext(DbContextOptions<MovieContext> opts)  
+        :base(opts)
+    {          
+
+    }
+
+    public DbSet<Movie> Movies { get; set; }
+}
